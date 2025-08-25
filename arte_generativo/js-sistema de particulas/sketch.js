cea903a9908c9cc9965756ab4dc13c8d9dbcf75d
Catalina F. Mora <catalinafullanamora@gmail.com>
2025-08-25 12:11:17 +0200

    sistema de particulas js terminado

--- arte_generativo/js-sistema de particulas/sketch.js
+++ arte_generativo/js-sistema de particulas/sketch.js
@@ -1,8 +1,13 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
+
+const width = 800;
+const height = 600;
+
 const settings = {
-    dimensions: [ 800, 600 ]
+    dimensions: [ width, height ],
+    animate: true
 };
 const sketch = ({context, width, height}) => {
     const agents = [];
@@ -11,15 +16,42 @@ const sketch = ({context, width, height}) => {
         const y = random.range(0, height);
         agents.push(new Agent(x, y));
     }
+
+
     return ({context, width, height}) => {
         context.fillStyle = '#FFF';
         context.fillRect(0, 0, width, height);
-        agents.forEach(agent => agent.draw(context));
+        
+        agents.forEach(agent => {
+            agent.update();
+            agent.draw(context);
+        });
+
+        for (let i = 0; i < agents.length; i++) {
+            const agent = agents[i];
+            for (let j = i + 1; j < agents.length; j++) {
+                const other = agents[j];
+                const dx = agent.position.x - other.position.x;
+                const dy = agent.position.y - other.position.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < 150) {
+                    context.strokeStyle = 'black';
+                    context.beginPath();
+                    context.moveTo(agent.position.x, agent.position.y);
+                    context.lineTo(other.position.x, other.position.y);
+                    context.stroke();
+                }
+            }
+        }
     };
 };
 
 canvasSketch(sketch, settings);
-class Point{
+
+
+
+class Vector{
     constructor(x, y){
         this.x = x;
         this.y = y;
@@ -28,13 +60,33 @@ class Point{
 
 class Agent{
     constructor(x, y){
-        this.position = new Point(x, y);
+        this.position = new Vector(x, y);
+        this.radius = Math.random() * 10 + 5;
+        this.velocity = new Vector(Math.random() * 2 - 1, Math.random() * 2 - 1);
     }
+
+    update(){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.bounce(width, height);
+    }
+    
+    bounce(width, height){
+        if(this.position.x < 20)   this.velocity.x *= -1;
+        if(this.position.x > width - 20)   this.velocity.x *= -1;
+        if(this.position.y < 20)   this.velocity.y *= -1;
+        if(this.position.y > height - 20)   this.velocity.y *= -1;
+    }
+
     draw(context){
-        context.fillStyle = '#000';
+        context.fillStyle = 'black';
+
+        context.save();
+        context.translate(this.position.x, this.position.y);
 
         context.beginPath();
-        context.arc(this.position.x + 5, this.position.y + 5, 5, 0, Math.PI * 2);
+        context.arc(0, 0, this.radius, 0, Math.PI * 2);
         context.fill();
+        context.restore();
     }
 }
